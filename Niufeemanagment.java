7dd030eb5cc53f262822b4584b7bef05a16dedc6
CodeWithDks <160503862+CodeWithDks@users.noreply.github.com>
2024-06-11 07:07:33 +0530

    Add files via upload

---
+++ Niufeemanagment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.niufeemanagment;
+
+/**
+ *
+ * @author Deepak Kumar
+ */
+public class Niufeemanagment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
